013798112d859e38c25e2839d26c605bba78a7cb
zakaria-bak <75492857+zakaria-bak@users.noreply.github.com>
2022-02-23 16:35:02 +0100

    Update views.py

--- website/views.py
+++ website/views.py
@@ -4,31 +4,30 @@ from werkzeug.utils import redirect
 from .models import  Note
 from . import db
 
+#creating a views bluprint
 views = Blueprint('views', __name__)
 
-@views.route("/", methods=["POST", "GET"])
+@views.route('/', methods=["GET", "POST"])
+@login_required
 def home():
     if request.method == 'POST':
-        note = request.form["note"]
+        note = request.form['note']
 
         if len(note) < 1:
-            flash('Note is short!', category='error')
+            flash('note too short!', category='error')
         else:
-            new_note = Note(data = note, user_id=current_user.id)
+            new_note = Note(data = note, user_id = current_user.id)
             db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
-        
-    return render_template("home.html", user = current_user)
+
+    return render_template('home.html', user=current_user)
 
 @views.route('/delete/<int:id>')
 def delete(id):
     note_to_delete = Note.query.get(int(id))
 
     if note_to_delete:
-        db.session.delete(note_to_delete)
-        db.session.commit()
-        return redirect('/')
-    
-    
-
+       db.session.delete(note_to_delete)
+       db.session.commit()
+       return redirect('/')
